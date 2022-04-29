a3fbe8c1086210c26e838de7d0afc6257210729b
nun2 <nun2@gmail.com>
2022-04-29 18:18:49 +0530

    making changes in GitPractice using IDE

--- src/main/java/student/GitPractice.java
+++ src/main/java/student/GitPractice.java
@@ -5,6 +5,7 @@ public class GitPractice {
 	{
 		System.out.println("practicing GIT");
 		System.out.println("Making changes using IDE");
-		
+		System.out.println("Making changes 2.0 using IDE");
+		System.out.println("Now in 2nd branch");
 	}
 }
